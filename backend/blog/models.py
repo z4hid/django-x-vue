f6faec6aa97e1033c2da93225cc8948be819ed86
z4hid <zahidhasan9297@gmail.com>
2025-03-11 15:59:17 +0600

    User Model added

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 class Site(models.Model):
     name = models.CharField(max_length=250)
@@ -12,3 +14,21 @@ class Site(models.Model):
     def __str__(self):
         return self.name
     
+
+class User(AbstractUser):
+    avatar = models.ImageField(
+        upload_to='user/avatars/%Y/%m/%d',
+        default='user/avatars/default.jpg',
+        blank=True, null=True)
+    
+    bio = models.TextField(max_length=500, null=True)
+    location = models.CharField(max_length=30, null=True)
+    website = models.CharField(max_length=100, null=True)
+    joined_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = '2. Users'
+
+    def __str__(self):
+        return self.username
