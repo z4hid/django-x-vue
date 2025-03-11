e3f1a03bbc8ed4ff87d48d003458d297ed60a326
z4hid <zahidhasan9297@gmail.com>
2025-03-11 15:53:09 +0600

    settings updated for media

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
